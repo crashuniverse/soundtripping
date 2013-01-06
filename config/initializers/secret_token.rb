4d80517e899184ea482f475c293d55fae592f49d
Ranjan <mail@priyaranjan.net>
2013-01-06 09:07:01 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundtripping::Application.config.secret_token = '1d05a2fd49c382b93d1707569dca5e050f05d50c3d976b83fabb5f1ae34f4996ada3bacabdc300ad3ad8f24f6d79f32cdacbae42de058e7f6a07fd322898e396'
